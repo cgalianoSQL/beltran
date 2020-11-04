866c4a0a25fd520915339a0f8de353085813663f
cgalianoSQL <you@example.com>
2020-11-04 14:13:02 -0300

    sd

--- frontEnd/servicioReporte.php
+++ frontEnd/servicioReporte.php
@@ -45,31 +45,53 @@ $perfil = $api->perfil($_SESSION['id']);
 		</nav>
 
 
-		<div id="colorcito1" class="container" >
-			<div class="row" >
-			
-				<div class="col col-lg-3" onclick="location.href='servicioreporte.php'" style="margin-top: 5%;margin-left: 20%;margin-bottom: 2%">
-					<div class="card" style="width: 18rem;">
-						<div class="card-body" style="min-width:286px;max-width: 286px;min-height:310px;max-height: 330px;">
-							
-							<div class="alert alert-secondary" role="alert">
-						  <h3>ESTADÍSTICAS DE SERVICIOS</h3>
-						</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="col col-lg-3" onclick="location.href='.php'" style="margin-top: 5%;margin-left: 5%;margin-bottom: 2%">
-					<div class="card" style="width: 18rem;">
-						<div class="card-body" style="min-width:286px;max-width: 286px;min-height:310px;max-height: 330px;">
-							<div class="alert alert-info" role="alert">
-						  <h3>ESTADÍSTICAS DE RECLAMOS</h3>
-						</div>
-						</div>
-					</div>
-				</div>
-			</div>
+		<center>
+		<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+		<div>
+		<canvas id="myChart" style = "width=20; height=20;"></canvas>
 		</div>
+
+		<script>
+		var ctx = document.getElementById('myChart').getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+				datasets: [{
+					label: '# of Votes',
+					data: [12, 19, 3, 5, 2, 3],
+					backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(255, 206, 86, 0.2)',
+						'rgba(75, 192, 192, 0.2)',
+						'rgba(153, 102, 255, 0.2)',
+						'rgba(255, 159, 64, 0.2)'
+					],
+					borderColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)',
+						'rgba(153, 102, 255, 1)',
+						'rgba(255, 159, 64, 1)'
+					],
+					borderWidth: 2
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero: true
+						}
+					}]
+				}
+			}
+		});
+		</script>
+		<center>
+
 	</div>
 </div>
 
