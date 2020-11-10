6fc841e55f8b24b41902f532f8c0070b9aa72776
cgalianoSQL <you@example.com>
2020-11-09 23:19:55 -0300

    bugfix

--- frontEnd/DetallesmisReclamos.php
+++ frontEnd/DetallesmisReclamos.php
@@ -10,6 +10,95 @@ include_once 'php/api/apiUser.php';
 $api = new ApiUser();
 $perfil = $api->perfil($_SESSION['id']);
 ?>
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+
+#myImg {
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+#myImg:hover {opacity: 0.7;}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+}
+
+/* Modal Content (image) */
+.modal-content {
+  margin: auto;
+  display: block;
+  width: 80%;
+  max-width: 700px;
+}
+
+/* Caption of Modal Image */
+#caption {
+  margin: auto;
+  display: block;
+  width: 80%;
+  max-width: 700px;
+  text-align: center;
+  color: #ccc;
+  padding: 10px 0;
+  height: 150px;
+}
+
+/* Add Animation */
+.modal-content, #caption {  
+  -webkit-animation-name: zoom;
+  -webkit-animation-duration: 0.6s;
+  animation-name: zoom;
+  animation-duration: 0.6s;
+}
+
+@-webkit-keyframes zoom {
+  from {-webkit-transform:scale(0)} 
+  to {-webkit-transform:scale(1)}
+}
+
+@keyframes zoom {
+  from {transform:scale(0)} 
+  to {transform:scale(1)}
+}
+
+/* The Close Button */
+.close {
+  position: absolute;
+  top: 15px;
+  right: 35px;
+  color: #f1f1f1;
+  font-size: 40px;
+  font-weight: bold;
+  transition: 0.3s;
+}
+
+.close:hover,
+.close:focus {
+  color: #bbb;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+/* 100% Image Width on Smaller Screens */
+@media only screen and (max-width: 700px){
+  .modal-content {
+    width: 100%;
+  }
+}
+</style>
 
 <!doctype html>
 <html lang="en">
@@ -91,25 +180,11 @@ $perfil = $api->perfil($_SESSION['id']);
                                                           ?>
                                                         <center>
 														<td>
-                                                            <img width="100" src="<?php ECHO json_decode(json_encode($r['archivo']))?> ">
-                                                              <!-- Botón en HTML (lanza el modal en Bootstrap) -->
-                                                             <a href="?id=ss#victorModal" role="button" class="btn btn-large btn-primary" data-toggle="modal">Ver Imagen </a>
-                                                    
-                                                             <!-- Modal / Ventana / Overlay en HTML -->
-                                                            <div id="victorModal" class="modal fade">
-                                                                <div class="modal-dialog">
-                                                                    <div class="modal-content">
-                                                                        <div class="modal-body">
-                                                                        <center>
-                                                                        <img width="400" src="<?php ECHO json_decode(json_encode($r['archivo']))?> ">
-                                                                        </center>
-                                                                            </div>
-                                                                        <div class="modal-footer">
-                                                                            <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar </button>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
+                                                        <form name="registro" action="/beltran/frontEnd//DetallesmisReclamos.php?id=<?php ECHO $_GET['id'] ?>&mostrar"  method="POST">
+                                                            <input type="hidden" name="id_imagen" value="<?php ECHO  json_decode(json_encode($r['archivo']));?> ">
+
+                                                           <button type="submit" ><img width="100" src="<?php ECHO json_decode(json_encode($r['archivo']))?> " alt="x" /></button>
+                                                        </form>
                                                         </td>
                                                         </center>
                                                           <?php
@@ -163,7 +238,48 @@ $perfil = $api->perfil($_SESSION['id']);
                 </div> 
             </div>  
         </div>   
-      
+
+
+<?php 
+    if (isset($_GET['mostrar'])){
+
+?>
+
+<img id="myImg" src="<?php ECHO $_POST['id_imagen']?>" alt="-" style="visibility: hidden; ">
+<!-- The Modal -->
+    <div id="myModal" class="modal">
+    <span class="close">&times;</span>
+    <img class="modal-content" id="img01">
+    <div id="caption"></div>
+    </div>
+
+    <script>
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById("myImg");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    img.onload  = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+    modal.style.display = "none";
+    }
+    </script>
+<?php 
+    }
+?>
+
+                
     <script src="jquery/jquery-3.3.1.min.js"></script>
     <script src="popper/popper.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
@@ -175,4 +291,5 @@ $perfil = $api->perfil($_SESSION['id']);
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
 </body>
 </html>
+
 
