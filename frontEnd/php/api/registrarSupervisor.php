b15a0c9bc14d49b6909e5b814eb7116417c290be
unknown <yonathan.malito@gmail.com>
2020-11-11 16:04:17 -0300

    Cambios varios

--- frontEnd/php/api/registrarSupervisor.php
+++ frontEnd/php/api/registrarSupervisor.php
@@ -48,7 +48,7 @@
             <script>     
             swal({
                 title: "SUPERVISOR CREADO CON ÉXITO",
-                text: "Puede visualizar el mismo en la sección “Lista de personal de soporte”",
+                text: "Puede visualizar el mismo en la sección “Lista de Supervisores",
                 icon: "success",
                 confirmButtonText: 'OK',
             }).then(function() {
